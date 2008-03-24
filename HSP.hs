cafbd5c2ae1e3a293771fbd01db665862f790e5b
Niklas Broberg <nibro@cs.chalmers.se>
2008-03-24 21:14:38 +0000

    Restructure of module system

---
+++ HSP.hs
@@ -0,0 +1,24 @@
+module HSP (
+        -- module HSP.Monad
+        HSP, runHSP, evalHSP,
+        getParam, getIncNumber, doIO, catch,
+        
+        -- module HSP.Env
+        module HSP.Env,
+        
+        -- module HSP.XML[.PCDATA]
+        module HSP.XML,
+        module HSP.XML.PCDATA,
+        
+        -- module HSP.XMLGenerator
+        module HSP.XMLGenerator
+
+    ) where
+
+import Prelude hiding (catch)
+
+import HSP.Monad
+import HSP.Env hiding (mkSimpleEnv)
+import HSP.XML hiding (Name)
+import HSP.XML.PCDATA
+import HSP.XMLGenerator
